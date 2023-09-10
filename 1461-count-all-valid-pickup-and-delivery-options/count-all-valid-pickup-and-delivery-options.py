f05b797bd51f05bf1c8caf17408f4af41b17cff0
adarshiremath <54905828+adarshiremath@users.noreply.github.com>
2023-09-10 13:37:28 -0700

    Time: 30 ms (96.17%) | Memory: 16.1 MB (99.57%) - LeetSync

---
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.py
@@ -0,0 +1,7 @@
+mod = 10**9 + 7
+class Solution:
+    def countOrders(self, n: int) -> int:
+        count = 1
+        for i in range(2, n + 1):
+            count = (count * (2 *i - 1) * i) % mod
+        return count
