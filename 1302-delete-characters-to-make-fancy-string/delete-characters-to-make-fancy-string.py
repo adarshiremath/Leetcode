f730ba1043a3924552b6ff5d27f74079e4f1e964
adarshiremath <54905828+adarshiremath@users.noreply.github.com>
2024-10-31 23:17:26 -0700

    Time: 6980 ms (5.41%) | Memory: 17.9 MB (95.86%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        i = 0
+        while i < len(s) - 2:
+            if s[i] == s[i+1] and s[i+1] == s[i+2]:
+                s = s[:i]+s[i+1:]
+                i -= 1
+            i += 1
+        return s
