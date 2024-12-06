7782b7dafd9584f4e0eec0fc924ff00fb949884b
arch-jet <54905828+arch-jet@users.noreply.github.com>
2024-12-06 11:49:40 -0700

    Time: 15 ms (91.50%) | Memory: 18.3 MB (28.73%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -1,8 +1,8 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        Max = -1
-        
-        for i in range(len(arr)-1, -1, -1):
+        Max = arr[-1]
+        arr[-1] = -1
+        for i in range(len(arr)-2, -1, -1):
             temp = arr[i]
             arr[i] = Max
             Max = temp if temp > Max else Max
