4c2b8c33f497255c54b42d9a78be9a5edfaf004d
arch-jet <54905828+arch-jet@users.noreply.github.com>
2024-12-06 12:42:54 -0700

    Time: 4 ms (8.25%) | Memory: 17.6 MB (7.77%) - LeetSync

---
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.py
@@ -0,0 +1,8 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        n = len(nums)//2
+        arr = []
+        for i in range(n):
+            freq, val = nums[2*i], nums[2*i + 1]
+            arr.extend([val]*freq)
+        return arr
