d4ed5b14372c2758e20ccfaf8227681f7e0f821d
arch-jet <54905828+arch-jet@users.noreply.github.com>
2024-12-16 17:11:49 -0700

    Time: 4 ms (58.72%) | Memory: 17.4 MB (10.05%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,12 +1,11 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_nums = sorted(nums)
+        dit = {}
         res = []
-        for i in range(len(nums)):
-            val = 0
-            for j in range(len(nums)):
-                if i != j:
-                    if nums[j] < nums[i]:
-                        val += 1
-            res.append(val)
-        
+        for i, num in enumerate(sorted_nums):
+            if num not in dit:
+                dit[num] = i
+        for num in nums:
+            res.append(dit[num])
         return res
