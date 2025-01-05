7d60cb0930717a629c4fd85720edccfc6c6bd415
arch-jet <54905828+arch-jet@users.noreply.github.com>
2025-01-05 10:26:35 -0700

    Time: 105 ms (55.02%) | Memory: 23.5 MB (21.49%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,21 +1,22 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        if len(nums) == 2:
-            if sum(nums) == 1:
-                return 2
-            else:
-                return 0
+        # if len(nums) == 2:
+        #     if sum(nums) == 1:
+        #         return 2
+        #     else:
+        #         return 0
         count = 0
         dit= {0:-1}
         Max = 0
         for i, x in enumerate(nums):
-            if x == 0:
-                count -= 1
-            elif x == 1:
-                count += 1
-            if count not in dit:
-                dit[count] = i
-            else:
+            count += 1 if x else -1
+            # if x == 0:
+            #     count -= 1
+            # elif x == 1:
+            #     count += 1
+            if count in dit:
                 Max = max(i - dit[count], Max)
+            else:
+                dit[count] = i
         
         return Max
