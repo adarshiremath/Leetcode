07c2187965ac0ab3dc4943c47b86f69e75483fdd
arch-jet <54905828+arch-jet@users.noreply.github.com>
2025-01-06 10:19:54 -0700

    Time: 15 ms (81.62%) | Memory: 17.8 MB (17.08%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dit = {}
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            if s[r] in dit:
+                l = max(dit[s[r]]+1, l)
+            dit[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
