79b9f93abf62e8994c75fc8da840323167eb19e1
arch-jet <54905828+arch-jet@users.noreply.github.com>
2025-01-06 18:18:05 -0700

    Time: 11 ms (24.91%) | Memory: 24.1 MB (7.03%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,9 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dit = {}
-        for i in nums: dit[i] = dit.get(i, 0) + 1
-        lis = []
-        for key, v in dit.items():
-            lis.append([key, v])
-        lis = list(zip(*sorted(lis, key=lambda x: x[1], reverse =True)))
-        return lis[0][:k]
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        
+        for n, c in count.items():
+            freq[c].append(n)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
