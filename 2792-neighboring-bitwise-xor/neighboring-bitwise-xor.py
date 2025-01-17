fc10ca7dd10a3654236e494048667bfa6ae98178
arch-jet <54905828+arch-jet@users.noreply.github.com>
2025-01-16 22:48:30 -0700

    Time: 17 ms (97.81%) | Memory: 22.4 MB (24.82%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -1,6 +1,3 @@
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        Sum = 0
-        for i in derived:
-            Sum = Sum ^ i
-        return False if Sum else True
+        return sum(derived)%2 == 0
