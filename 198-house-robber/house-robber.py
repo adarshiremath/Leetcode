6efada9c123cf486f20a589735c91e6de498527f
adarshiremath <54905828+adarshiremath@users.noreply.github.com>
2025-03-11 14:09:56 -0700

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.71%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,32 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        prev = nums[0]
-        prev2 = 0
+        # prev = nums[0]
+        # prev2 = 0
+        # for n in range(1, len(nums)):
+        #     left = nums[n]
+        #     if n > 1:
+        #         left += prev2
+        #     right = 0 + prev
+
+        #     cur = max(left, right)
+        #     prev2 = prev
+        #     prev = cur
+
+        # return prev
+
+        dp = [-1] * len(nums)
+        dp[0] = nums[0]
+
         for n in range(1, len(nums)):
             left = nums[n]
             if n > 1:
-                left += prev2
-            right = 0 + prev
+                left += dp[n - 2]
+            right = 0 + dp[n - 1]
 
-            cur = max(left, right)
-            prev2 = prev
-            prev = cur
+            dp[n] = max(left, right)
 
-        return prev
-
-        # dp = [-1] * len(nums)
-        # dp[0] = nums[0]
-
-        # for n in range(1, len(nums)):
-        #     left = 0
-        #     if n > 1:
-        #         left = nums[n] + dp[n - 2]
-        #     right = 0 + dp[n - 1]
-
-        #     dp[n] = max(left, right)
-
-        # return dp[len(nums)-1]
+        return dp[len(nums)-1]
         # def fun(n):
         #     if n == 0:
         #         return nums[0]
